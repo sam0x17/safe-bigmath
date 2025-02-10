a707ccd8582f7ba83bb18692b888c050ef8cdcb7
Sam Johnson <sam@durosoft.com>
2025-02-09 23:39:40 -0500

    use uintx

--- src/heapint.rs
+++ src/heapint.rs
@@ -1,5 +1,7 @@
+use uintx::{u120, u56};
+
 #[repr(align(16))]
 pub enum HeapInt {
-    U120([u8; 15]),
-    Ptr { ptr: *mut u8, len: [u8; 7] },
+    U120(u120),
+    Ptr { ptr: *mut u8, len: u56 },
 }
