7b19caae802a37d3e6d4c13d5a2f3c5b6e10ed3c
Sam Johnson <sam@durosoft.com>
2025-02-09 21:43:33 -0500

    fix

--- src/better.rs
+++ src/better.rs
@@ -1,11 +1,5 @@
-#[repr(C)]
+#[repr(align(16))]
 pub enum HeapInt {
     U120([u8; 15]),
-    Ptr { len: [u8; 7], ptr: *mut u8 },
-}
-
-impl HeapInt {
-    pub const fn from_u8(val: u8) -> Self {
-        HeapInt::U120([val; 15])
-    }
+    Ptr { ptr: *mut u8, len: [u8; 7] },
 }
