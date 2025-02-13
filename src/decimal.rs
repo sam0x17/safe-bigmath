523f498f29fb2ad83838223c160b47f2ce8d7f26
Sam Johnson <sam@durosoft.com>
2025-02-13 02:19:24 -0500

    clean up

--- src/decimal.rs
+++ src/decimal.rs
@@ -1,5 +1,4 @@
-use core::str::FromStr;
-
+use core::{ops::*, str::FromStr};
 use quoth::Parsable;
 
 use crate::{parsing::ParsedSafeDec, SafeInt};
@@ -10,6 +9,10 @@ pub struct SafeDec<const D: usize>(SafeInt);
 
 impl<const D: usize> SafeDec<D> {
     pub const ZERO: SafeDec<D> = SafeDec::<D>(SafeInt::ZERO);
+
+    fn scale_up(other: &SafeInt) -> SafeInt {
+        other * SafeInt::from(10).pow(D as u32)
+    }
 }
 
 impl<const D: usize> FromStr for SafeDec<D> {
@@ -22,10 +25,92 @@ impl<const D: usize> FromStr for SafeDec<D> {
     }
 }
 
+macro_rules! impl_binary_op {
+    ($trait:ident, $method:ident) => {
+        impl<const D: usize> $trait for SafeDec<D> {
+            type Output = SafeDec<D>;
+
+            #[inline(always)]
+            fn $method(self, other: SafeDec<D>) -> SafeDec<D> {
+                SafeDec(self.0.$method(other.0))
+            }
+        }
+
+        impl<const D: usize> $trait<&SafeDec<D>> for &SafeDec<D> {
+            type Output = SafeDec<D>;
+
+            #[inline(always)]
+            fn $method(self, other: &SafeDec<D>) -> SafeDec<D> {
+                SafeDec(self.0.clone().$method(&other.0))
+            }
+        }
+
+        impl<const D: usize> $trait<&SafeDec<D>> for SafeDec<D> {
+            type Output = SafeDec<D>;
+
+            #[inline(always)]
+            fn $method(self, other: &SafeDec<D>) -> SafeDec<D> {
+                SafeDec(self.0.$method(&other.0))
+            }
+        }
+
+        impl<const D: usize> $trait<SafeDec<D>> for &SafeDec<D> {
+            type Output = SafeDec<D>;
+
+            #[inline(always)]
+            fn $method(self, other: SafeDec<D>) -> SafeDec<D> {
+                SafeDec(self.0.clone().$method(other.0))
+            }
+        }
+
+        impl<const D: usize> $trait<&SafeDec<D>> for &SafeInt {
+            type Output = SafeDec<D>;
+
+            #[inline(always)]
+            fn $method(self, other: &SafeDec<D>) -> SafeDec<D> {
+                SafeDec(SafeDec::<D>::scale_up(self).$method(&other))
+            }
+        }
+
+        impl<const D: usize> $trait<&SafeDec<D>> for SafeInt {
+            type Output = SafeDec<D>;
+
+            #[inline(always)]
+            fn $method(self, other: &SafeDec<D>) -> SafeDec<D> {
+                SafeDec(SafeDec::<D>::scale_up(&self).$method(&other))
+            }
+        }
+
+        impl<const D: usize> $trait<SafeDec<D>> for &SafeInt {
+            type Output = SafeDec<D>;
+
+            #[inline(always)]
+            fn $method(self, other: SafeDec<D>) -> SafeDec<D> {
+                SafeDec(SafeDec::<D>::scale_up(self).$method(other.0))
+            }
+        }
+
+        impl<const D: usize> $trait<SafeDec<D>> for u8 {
+            type Output = SafeDec<D>;
+
+            #[inline(always)]
+            fn $method(self, other: SafeDec<D>) -> SafeDec<D> {
+                SafeDec(SafeInt::from(self).$method(other.0))
+            }
+        }
+    };
+}
+
+impl_binary_op!(Add, add);
+
 #[test]
 fn test_safe_dec_from_str() {
     let parsed = "123.456".parse::<SafeDec<3>>().unwrap();
     assert_eq!(parsed.0, SafeInt::from(123456));
     let parsed = "123.456".parse::<SafeDec<4>>().unwrap();
     assert_eq!(parsed.0, SafeInt::from(1234560));
+    let other = SafeDec::<4>::scale_up(&SafeInt::from(100));
+    assert_eq!(other, SafeInt::from(100_0000));
+    let parsed = other + parsed;
+    assert_eq!(parsed.0, SafeInt::from(1234560 + 100_0000));
 }
